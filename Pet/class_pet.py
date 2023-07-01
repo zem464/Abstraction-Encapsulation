3775a85e0b7d696a4a52c41fbf863bd679d4e2ab
Iris Membrere <irismembrere@gmail.com>
2023-07-01 18:01:59 +0800

    Pseudocode for the 'Pet' exercise

---
+++ Pet/class_pet.py
@@ -0,0 +1,13 @@
+# Create the class instance
+
+# Create the initializing constructor
+
+# Create the setter for the attributes
+
+# Create the getter for the attibutes
+
+# Create the object
+
+# Ask for inputs
+
+# Print the information
