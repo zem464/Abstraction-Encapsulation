2f40a9f2ae4ecc4d3664aa435ae0b9487da8f2d1
Iris Membrere <irismembrere@gmail.com>
2023-07-01 19:36:08 +0800

    Adding colors

--- Car/TestCar.py
+++ Car/TestCar.py
@@ -14,7 +14,7 @@ for _ in range(5):
     print("\n\033[33;40m\033[1mCurrent speed: \033[31m\033[0m", car.get_speed())
 
 # Print the braking information
-print("\n[\033[35;40m\033[1m     BRAKE    \033[31m\033[0m]")
+print("\n[\033[35;40m\033[1m     BRAKE     \033[31m\033[0m]")
 print("\n\033[34;40m\033[1mCar year model: \033[31m\033[0m", car.get_year_model())
 print("\033[34;40m\033[1mCar made of: \033[31m\033[0m", car.get_make())
 print("\n\033[37;40m\033[1m<<<  SPEED   >>>\033[0m")
