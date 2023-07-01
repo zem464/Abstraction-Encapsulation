87adb5f0d49d04bd08f43a4197cc673779e9ccc3
Iris Membrere <irismembrere@gmail.com>
2023-07-01 19:44:14 +0800

    Creating and Displaying the function

--- Fan/TestFan.py
+++ Fan/TestFan.py
@@ -2,9 +2,23 @@
 from class_fan import Fan
 
 # Create a defining function for the objects
+def TestFan():
+    fan1 = Fan(Fan.fast, 10, "Yellow", True)
+    fan2 = Fan(Fan.medium, 5, "Blue", False)
 
-# Display fan1
+    # Display fan1
+    print("[First Fan]")
+    print("Speed: ", fan1.get_speed())
+    print("Radius: ", fan1.get_radius())
+    print("Color: ", fan1.get_color())
+    print("On: ", fan1.get_fan_on())
 
-# Display fan2
+    # Display fan2
+    print("[Second Fan]")
+    print("Speed: ", fan2.get_speed())
+    print("Radius: ", fan2.get_radius())
+    print("Color: ", fan2.get_color())
+    print("On: ", fan2.get_fan_on())
 
 # Call the main function
+TestFan()
--- Fan/class_fan.py
+++ Fan/class_fan.py
@@ -50,26 +50,3 @@ class Fan:
 
     def fan_on(self):
         self.fan_on = False
-
-# Construct a program for the functions for the fan1 and fan2
-class TestFan():
-    def main(self):
-        fan1 = Fan(Fan.fast, 10, "Yellow", True)
-        fan2 = Fan(Fan.medium, 5, "Blue", False)
-
-        # Display fan1
-        print("[First Fan]")
-        print("Speed: ", fan1.get_speed())
-        print("Radius: ", fan1.get_radius())
-        print("Color: ", fan1.get_color())
-        print("On: ", fan1.get_fan_on())
-
-        # Display fan2
-        print("[Second Fan]")
-        print("Speed: ", fan2.get_speed())
-        print("Radius: ", fan2.get_radius())
-        print("Color: ", fan2.get_color())
-        print("On: ", fan2.get_fan_on())
-
-# Close the program by creating an instance method
-TestFan().main()
