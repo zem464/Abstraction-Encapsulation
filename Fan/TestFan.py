38379d2d4437c071197b854298802fd66086e265
Iris Membrere <irismembrere@gmail.com>
2023-07-01 14:22:31 +0800

    Printing the display for fan1

--- Fan/TestFan.py
+++ Fan/TestFan.py
@@ -7,7 +7,12 @@ class TestFan(Fan):
         fan1 = Fan(Fan.fast, 10, "Yellow", True)
         fan2 = Fan(Fan.medium, 5, "Blue", False)
 
-# Display fan1
+        # Display fan1
+        print("[First Fan]")
+        print("Speed: ", fan1.get_speed())
+        print("Radius: ", fan1.get_radius())
+        print("Color: ", fan1.get_color())
+        print("On: ", fan1.get_fan_on())
 
 # Display fan2
 
