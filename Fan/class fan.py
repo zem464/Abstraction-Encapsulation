ac057526d3d87f5566284603faeb5c559adb645b
Iris Membrere <irismembrere@gmail.com>
2023-07-01 14:00:52 +0800

    Creating a constructor method for the data field

--- Fan/class fan.py
+++ Fan/class fan.py
@@ -4,8 +4,13 @@ class Fan:
     slow = 1
     medium = 2
     fast = 3
-    
-# Initialize the needed data field
+
+    # Initialize the needed data field
+    def __init__(self, speed, radius, color, fan_on):
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__fan_on = fan_on
 
 # Create the getters
 
