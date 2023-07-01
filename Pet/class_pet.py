c878074e28d0378cfc311531b2b39141c00ef642
Iris Membrere <irismembrere@gmail.com>
2023-07-01 18:06:46 +0800

    Create the setter for the private attributes

--- Pet/class_pet.py
+++ Pet/class_pet.py
@@ -6,7 +6,15 @@ class Pet:
             self.__animal_type = ""
             self.__age = 0
 
-# Create the setter for the attributes
+    # Create the setter for the attributes
+    def set_name(self, name):
+        self.__name = name
+    
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
 
 # Create the getter for the attibutes
 
