091fb02329f21302a454dfa165610f2b470586d4
Iris Membrere <irismembrere@gmail.com>
2023-07-01 13:36:53 +0800

    Pseudocode for 'Fan' exercise

---
+++ Fan/TestFan.py
@@ -0,0 +1,9 @@
+# Import the class fan
+
+# Construct a program for the functions for the fan1 and fan2
+
+# Display fan1
+
+# Display fan2
+
+# Close the program
---
+++ Fan/class fan.py
@@ -0,0 +1,11 @@
+# Create class fan
+
+# Create the constants
+
+# Initialize the needed data field
+
+# Create the getters
+
+# Create the setters
+
+# Create methods for the default values
