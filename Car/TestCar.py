e4a21b47d503924ecea44d3dcdb59f95ee9a76f9
Iris Membrere <irismembrere@gmail.com>
2023-07-01 19:20:54 +0800

    Creating the object and printing the acceleration and brake

--- Car/TestCar.py
+++ Car/TestCar.py
@@ -1,7 +1,21 @@
 # Import class
+from class_car import Car
 
 # Create the object
+car = Car(2013, 'Hyundai', 0)
 
 # Print the accelerating information 
+print("ACCELERATION")
+print("\nCar year model: ", car.get_year_model())
+print("Car made of: ", car.get_make())
+for _ in range(5):
+    car.accelerate()
+    print("Current speed: ", car.get_speed())
 
 # Print the braking information
+print("\nBRAKE")
+print("\nCar year model: ", car.get_year_model())
+print("Car made of: ", car.get_make())
+for _ in range(5):
+    car.brake()
+    print("Current speed: ", car.get_speed())
