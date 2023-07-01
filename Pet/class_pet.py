75f2664498f31a54d7b6092d6d8e3b81df67d382
Iris Membrere <irismembrere@gmail.com>
2023-07-01 18:12:21 +0800

    Print the information

--- Pet/class_pet.py
+++ Pet/class_pet.py
@@ -35,3 +35,7 @@ pet.set_animal_type(input("What type of animal is your pet?: "))
 pet.set_age(input("How old is your pet?(in months): "))
 
 # Print the information
+print("\n[PET'S INFORMATION]")
+print("Name: ", pet.get_name())
+print("Animal Type: ", pet.get_animal_type())
+print("Age: ", pet.get_age())
