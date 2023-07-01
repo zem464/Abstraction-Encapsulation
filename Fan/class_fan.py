cfb2301d000823aa63d9d502203b034bfa3662e2
Iris Membrere <irismembrere@gmail.com>
2023-07-01 14:35:07 +0800

    Fixing class error

--- Fan/class_fan.py
+++ Fan/class_fan.py
@@ -52,7 +52,7 @@ class Fan:
         self.fan_on = False
 
 # Construct a program for the functions for the fan1 and fan2
-class TestFan(Fan):
+class TestFan():
     def main(self):
         fan1 = Fan(Fan.fast, 10, "Yellow", True)
         fan2 = Fan(Fan.medium, 5, "Blue", False)
