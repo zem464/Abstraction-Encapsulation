ddabe3a4c34524e5de112904116648bb2fab6554
Iris Membrere <irismembrere@gmail.com>
2023-07-01 16:55:53 +0800

    Pseudocode for the 'Car' exercise

---
+++ Car/class_car.py
@@ -0,0 +1,15 @@
+# Create the class for the car
+
+# Initialize the needed attributes
+
+# Create the method for acceleration
+
+# Create the method for braking
+
+# Create the getter for the attributes
+
+# Create the object
+
+# Create the accelerating information 
+
+# Create the braking information
