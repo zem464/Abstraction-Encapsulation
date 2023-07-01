8cbdc6436ca9c9f1508dc793fef76463c4869e21
Iris Membrere <irismembrere@gmail.com>
2023-07-01 17:39:37 +0800

    Initializing the attributes

--- Car/class_car.py
+++ Car/class_car.py
@@ -1,6 +1,10 @@
 # Create the class for the car
 class Car:
-# Initialize the needed attributes
+    # Initialize the needed attributes
+    def __init__(self, year_model, make, speed):
+            self.__year_model = year_model
+            self.__make = make
+            self.__speed = speed
 
 # Create the method for acceleration
 
