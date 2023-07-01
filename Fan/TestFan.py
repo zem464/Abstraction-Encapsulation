81cc8d0a3c36e1c3ff68fe16026288f92143fc2b
Iris Membrere <irismembrere@gmail.com>
2023-07-01 14:21:05 +0800

    Creating the program for TestFan

--- Fan/TestFan.py
+++ Fan/TestFan.py
@@ -2,6 +2,10 @@
 from class_fan import Fan
 
 # Construct a program for the functions for the fan1 and fan2
+class TestFan(Fan):
+    def main(self):
+        fan1 = Fan(Fan.fast, 10, "Yellow", True)
+        fan2 = Fan(Fan.medium, 5, "Blue", False)
 
 # Display fan1
 
