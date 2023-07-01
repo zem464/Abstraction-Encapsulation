6fd9820a11c84b3ca61bdbafc372befaecf363e8
Iris Membrere <irismembrere@gmail.com>
2023-07-01 19:34:43 +0800

    Removing the testcar variables from the classcar:

--- Car/TestCar.py
+++ Car/TestCar.py
@@ -5,17 +5,19 @@ from class_car import Car
 car = Car(2013, 'Hyundai', 0)
 
 # Print the accelerating information 
-print("ACCELERATION")
-print("\nCar year model: ", car.get_year_model())
-print("Car made of: ", car.get_make())
+print("\n[\033[35;40m\033[1m    ACCELERATION    \033[31m\033[0m]")
+print("\n\033[34;40m\033[1mCar year model: \033[31m\033[0m", car.get_year_model())
+print("\033[34;40m\033[1mCar made of: \033[31m\033[0m", car.get_make())
+print("\n\033[37;40m\033[1m<<<  SPEED   >>>\033[0m")
 for _ in range(5):
     car.accelerate()
-    print("Current speed: ", car.get_speed())
+    print("\n\033[33;40m\033[1mCurrent speed: \033[31m\033[0m", car.get_speed())
 
 # Print the braking information
-print("\nBRAKE")
-print("\nCar year model: ", car.get_year_model())
-print("Car made of: ", car.get_make())
+print("\n[\033[35;40m\033[1m     BRAKE    \033[31m\033[0m]")
+print("\n\033[34;40m\033[1mCar year model: \033[31m\033[0m", car.get_year_model())
+print("\033[34;40m\033[1mCar made of: \033[31m\033[0m", car.get_make())
+print("\n\033[37;40m\033[1m<<<  SPEED   >>>\033[0m")
 for _ in range(5):
     car.brake()
-    print("Current speed: ", car.get_speed())
+    print("\n\033[31;40m\033[1mCurrent speed: \033[31m\033[0m", car.get_speed())
--- Car/class_car.py
+++ Car/class_car.py
@@ -25,22 +25,3 @@ class Car:
 
     def get_speed(self):
         return self.__speed
-
-# Create the object
-car = Car(2013, 'Hyundai', 0)
-
-# Print the accelerating information 
-print("ACCELERATION")
-print("\nCar year model: ", car.get_year_model())
-print("Car made of: ", car.get_make())
-for _ in range(5):
-    car.accelerate()
-    print("Current speed: ", car.get_speed())
-
-# Print the braking information
-print("\nBRAKE")
-print("\nCar year model: ", car.get_year_model())
-print("Car made of: ", car.get_make())
-for _ in range(5):
-    car.brake()
-    print("Current speed: ", car.get_speed())
