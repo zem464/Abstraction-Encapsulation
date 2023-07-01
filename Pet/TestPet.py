6ba656e5f4f88d99fee6f472d0079b02c0c7ff4f
Iris Membrere <irismembrere@gmail.com>
2023-07-01 18:50:02 +0800

    Creating the object, asking for the inputs, and printing of the information

--- Pet/TestPet.py
+++ Pet/TestPet.py
@@ -2,7 +2,15 @@
 from class_pet import Pet
 
 # Create the object
+pet = Pet()
 
 # Ask for inputs
+pet.set_name(input("What is the name of your pet?: "))
+pet.set_animal_type(input("What type of animal is your pet?: "))
+pet.set_age(input("How old is your pet?(in months): "))
 
 # Print the information
+print("\n[PET'S INFORMATION]")
+print("Name: ", pet.get_name())
+print("Animal Type: ", pet.get_animal_type())
+print("Age: ", pet.get_age(), "- month old")
